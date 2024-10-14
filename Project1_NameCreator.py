811db4efc4eec6fe9bfe964db918504e6e9a8a49
anabarrueco <155362077+anabg13@users.noreply.github.com>
2024-10-14 18:13:01 +0200

    Update Project1_NameCreator.py

--- Project1_NameCreator.py
+++ Project1_NameCreator.py
@@ -1,5 +1,13 @@
 print('Question 1:')
 ans1 = input('What is you favourite color?: ')
+if not ans1.isalpha():
+    print('Error. You must introduce a word.')
+    exit()
+
 print('Question 2:')
 ans2 = input('What city would you like to visit soon?: ')
+if not ans2.isalpha():
+    print('Error. You must introduce a word.')
+    exit()
+
 print('Your brand name is: "' + ans1 + ' ' + ans2 + '"' '\nHope you like it.')
